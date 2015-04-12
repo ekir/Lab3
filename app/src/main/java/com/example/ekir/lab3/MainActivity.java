32399aa89b1974d8636d2b8030d1d2cd0baa1ad3
ekir <ekir.gustafsson@gmail.com>
2015-04-12 21:50:24 +0200

    Music

--- app/src/main/java/com/example/ekir/lab3/MainActivity.java
+++ app/src/main/java/com/example/ekir/lab3/MainActivity.java
@@ -1,28 +1,47 @@
 package com.example.ekir.lab3;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 
 public class MainActivity extends ActionBarActivity {
     Button btn_play;
     Button btn_about;
+    CheckBox chk_music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final MediaPlayer MediaSpelaren = MediaPlayer.create(this, R.raw.music);
+        MediaSpelaren.setLooping(true);
+        MediaSpelaren.start();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_play= (Button) findViewById(R.id.btn_play);
         btn_about= (Button) findViewById(R.id.btn_about);
+        chk_music = (CheckBox) findViewById(R.id.chk_music);
         btn_about.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AboutActivity.class);
                 MainActivity.this.startActivity(intent);
+            }
+        });
+        chk_music.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    MediaSpelaren.start();
+                } else {
+                    MediaSpelaren.pause();
+                }
             }
         });
         btn_play.setOnClickListener(new View.OnClickListener() {
